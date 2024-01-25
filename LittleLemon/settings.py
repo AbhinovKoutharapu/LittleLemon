1dd8a4d4125171d6029ad7a8b315d6465053f7d6
Abhinov Koutharapu <abhinovkoutharapu@abhinovs-mbp.lan>
2024-01-24 23:30:19 -0600

    Added Auth

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "restaurant"
+    "restaurant",
+    "djoser",
+    'rest_framework.authtoken',
+    
 ]
 
 MIDDLEWARE = [
@@ -131,3 +134,16 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+DJOSER={"USER_ID_FIELD":"username"}
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
--- restaurant/views.py
+++ restaurant/views.py
@@ -6,6 +6,7 @@ from .models import Menu, Booking
 
 from .serializers import MenuSerializer, BookingSerializer, UserSerializer
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 def index(request):
@@ -22,5 +23,6 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
 
 
